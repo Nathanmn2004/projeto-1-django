3e0e27948978db0a06a33e7fc565d66456d21900
Nathan <nathanmeira10@gmail.com>
2024-06-17 15:07:28 -0300

    Criação do app recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from recipes.views import sobre,contato
+
+def my_view(request):
+    return HttpResponse('HOME')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/' , sobre),
+    path('', my_view),
+    path('contato', contato),
 ]
